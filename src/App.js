10a7bf82585efc3092d670b12de88036c448eeca
gg134679852 <metal852794613@gmail.com>
2022-03-21 11:20:00 +0800

    update-DataButton

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState ,useEffect} from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import DataTable from './components/DataTable';
 import LineChart from './components/LineChart';
 import DataButton from './components/DataButton';
@@ -7,24 +7,33 @@ import './App.css';
 
 function App() {
   const [marketData, setMarkData] = useState()
-  const getMarkData = ()=>{
+  const [currentData, setCurrentData] = useState("3month_Tbill")
+  const getMarkData = () => {
     axios.get("https://api.loc-cse.fst.network/ken/market_data")
-    .then((obj)=>{
-      setMarkData(obj.data.market_data)
-    })
+      .then((obj) => {
+        setMarkData(obj.data.market_data)
+      })
   }
-  useEffect(()=>{
+  const addCurrentData= (e) => {
+    setCurrentData(e.target.innerText)
+  }
+  useEffect(() => {
     getMarkData()
-  },[])
+  }, [])
   return (
     <div className="App">
-      <div className="App-button-area">
-        <DataButton />
-      </div>
-      <div className="App-data-area">
-        <DataTable />
-        <LineChart />
-      </div>
+      {
+        marketData && Object.keys(marketData).length ?
+        <Fragment>
+          <div className="App-button-area">
+              <DataButton addCurrentData={addCurrentData}/>
+          </div>
+          <div className="App-data-area">
+              <DataTable marketDataName={currentData} marketData={marketData[currentData]} />
+            <LineChart marketData={marketData[currentData]} />
+          </div>
+        </Fragment>: <h1>loading.....</h1>
+      }
     </div>
   );
 }

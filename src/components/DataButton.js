6c6a519bb9387a2e363cf0a7cabb207ea5f81623
gg134679852 <metal852794613@gmail.com>
2022-03-21 10:08:30 +0800

    src-update

---
+++ src/components/DataButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DataButton = () => {
+  return (
+    <div>
+      <button type="button" className="btn btn-primary">Primary</button>
+    </div>
+  )
+}
+
+export default DataButton
